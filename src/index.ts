eb96dedc7f91a9bd64d74575c7a4c197dd59b76e
Gergo <gege251@gmail.com>
2019-03-23 11:09:41 +0100

    fix index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as string from "./string"
+import * as number from "./number"
 import * as validatee from "./validatee"
 
-export { validatee, string }
+export { validatee, string, number }
