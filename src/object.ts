4e19a7564d12f920d863ac01733b0fc227f9d599
Gergo <gege251@gmail.com>
2019-04-19 12:34:35 +0900

    update object schema form checking

--- src/object.ts
+++ src/object.ts
@@ -4,11 +4,21 @@ type Schema<T> = { [K in keyof T]: Validator<T[K]> }
 
 /** Creates a validator that checks the properties of an object based on a schema */
 const schema = <T extends Object>(
+  errorMsg: string,
   schema: Schema<T>
 ): Validator<T> => validatee =>
   Object.entries(validatee.value).reduce(
     (prevObj, [key, value]) => merge(prevObj, validate(value, schema[key])),
-    succeed(validatee)
+    schemaForm(errorMsg, schema)(validatee)
   )
+
+/** Creates a validator that checks if the object type matches the schema */
+const schemaForm = (
+  errorMsg: string,
+  schema: Schema<any>
+): Validator<any> => validatee =>
+  Object.keys(schema).every(key => typeof validatee.value[key] !== "undefined")
+    ? succeed(validatee)
+    : fail(errorMsg)(validatee)
 
 export { schema }
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -1,18 +1,17 @@
 import { schema } from "../src/object"
 import { init } from "../src/validatee"
 import * as string from "../src/string"
-import { stringTypeAnnotation } from "@babel/types"
 
 describe("object validators", () => {
   describe("schema validator", () => {
     describe("flat schema", () => {
-      const schemaValidator = schema({
+      const schemaValidator = schema("invalid object", {
         fieldA: string.max("too long", 4),
         fieldB: string.min("too short", 4),
         fieldC: string.onlyNumbers("not a number")
       })
 
-      it("succeeds when every element of the array succeeds", () =>
+      it("succeeds when every property of the object succeeds", () =>
         expect(
           schemaValidator(
             init({
@@ -23,7 +22,17 @@ describe("object validators", () => {
           ).isValid
         ).toBeTruthy())
 
-      it("fails when any element of the array fails", () =>
+      it("fails when the object type doesn't match the schema", () =>
+        expect(
+          schemaValidator(
+            init<any>({
+              fieldA: "1234",
+              fieldB: "1234"
+            })
+          ).errors
+        ).toEqual(["invalid object"]))
+
+      it("fails when any property of the object fails", () =>
         expect(
           schemaValidator(
             init({
@@ -33,19 +42,30 @@ describe("object validators", () => {
             })
           ).isValid
         ).toBeFalsy())
+
+      it("aggregates error messages when many properties of the object fail", () =>
+        expect(
+          schemaValidator(
+            init({
+              fieldA: "1234",
+              fieldB: "123",
+              fieldC: "1234a"
+            })
+          ).errors
+        ).toEqual(["too short", "not a number"]))
     })
 
     describe("nested schema", () => {
-      const schemaValidator = schema({
+      const schemaValidator = schema("invalid object", {
         fieldA: string.max("too long", 4),
         fieldB: string.min("too short", 4),
-        fieldC: schema({
+        fieldC: schema("invalid object", {
           nestedFieldA: string.onlyNumbers("not a number"),
           nestedFieldB: string.onlyLowcases("not a lowcase")
         })
       })
 
-      it("succeeds when every element of the array succeeds", () =>
+      it("succeeds when every property of the object succeeds", () =>
         expect(
           schemaValidator(
             init({
@@ -59,7 +79,7 @@ describe("object validators", () => {
           ).isValid
         ).toBeTruthy())
 
-      it("fails when any element of the array fails", () =>
+      it("fails when any property of the object fails", () =>
         expect(
           schemaValidator(
             init({
@@ -72,6 +92,20 @@ describe("object validators", () => {
             })
           ).isValid
         ).toBeFalsy())
+
+      it("aggregates error messages when many properties of the object fail", () =>
+        expect(
+          schemaValidator(
+            init({
+              fieldA: "12345",
+              fieldB: "1234",
+              fieldC: {
+                nestedFieldA: "123",
+                nestedFieldB: "abc123"
+              }
+            })
+          ).errors
+        ).toEqual(["too long", "not a lowcase"]))
     })
   })
 })
